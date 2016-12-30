4d07008d87275a5cced193d32ef5bc6e101bae4f
Jeremy Pierre <j.14159@gmail.com>
2016-12-30 09:42:36 -0800

    Single multiple module AST generation function
    make_modules/1 takes a list of source code strings to parse and rewrite
    as proper modules with imports and exports expanded.  Expanded exports
    now work so we can do things like `export foo` instead of `export foo/1,
    foo/2`.  Rewriting functions not defined in the module to inter-module
    calls using imports is not yet done but I think because of the
    increasing amount of changes it's worth integrating now, if only to get
    the export changes in.
    
    Fixes #63

--- src/alpaca_ast_gen.erl
+++ src/alpaca_ast_gen.erl
@@ -103,6 +103,7 @@ expand_exports([M|Tail], Memo) ->
                                 } <- M#alpaca_module.functions, N =:= Name]
         end,
     Exports = lists:flatten(lists:map(F, M#alpaca_module.function_exports)),
+
     expand_exports(Tail, [M#alpaca_module{function_exports=Exports}|Memo]).
 
 %% Assumes that expand_exports has already been run on the supplied modules.
