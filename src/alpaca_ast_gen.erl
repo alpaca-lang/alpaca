05f1024e52beaffc008aecbab96f14e4b20ef35f
Dave Jeffrey <mail@davidjeffrey.co.uk>
2016-12-31 09:32:27 +0000

    All tests now pass

--- src/alpaca_ast_gen.erl
+++ src/alpaca_ast_gen.erl
@@ -865,7 +865,7 @@ defn_test_() ->
                      {alpaca_apply,undefined,1,
                        {symbol,1,"sideEffectingFun"},
                        [{int,1,5}]}}},
-        parse(alpaca_scanner:scan("x=sideEffectingFun 5"))),
+        parse(alpaca_scanner:scan("let x=sideEffectingFun 5"))),
      ?_assertMatch(
         {ok, {error, non_literal_value, {symbol, 1, "x"},                  
                          {alpaca_record,2,1,false,
@@ -875,7 +875,7 @@ defn_test_() ->
                                   {alpaca_apply,undefined,1,
                                       {symbol,1,"sideEffectingFun"},
                                       [{int,1,5}]}}]}}},
-        parse(alpaca_scanner:scan("x={one = 10, two = (sideEffectingFun 5)}"))),        
+        parse(alpaca_scanner:scan("let x={one = 10, two = (sideEffectingFun 5)}"))),        
      ?_assertMatch(
         {ok, {error, non_literal_value, {symbol, 1, "x"}, 
                      {alpaca_cons,undefined,0,
@@ -885,7 +885,7 @@ defn_test_() ->
                                      {symbol,1,"sideEffectingFun"},
                                      [{int,1,5}]},
                                  {nil,0}}}}},
-        parse(alpaca_scanner:scan("x=[1, (sideEffectingFun 5)]"))),        
+        parse(alpaca_scanner:scan("let x=[1, (sideEffectingFun 5)]"))),        
      ?_assertMatch(
         {ok, 
          #alpaca_fun_def{name={symbol, 1, "double"},
