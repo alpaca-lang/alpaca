ac80ed91c0ad5efc418497cd838dd0171f489135
Daniel Abrahamsson <hamsson@gmail.com>
2017-02-14 21:27:25 +0100

    Rename alpaca_error_fmt to alpaca_error_format.

--- src/alpaca_error_format.erl
+++ src/alpaca_error_format.erl
@@ -9,7 +9,7 @@
 %%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %%% See the License for the specific language governing permissions and
 %%% limitations under the License.
--module(alpaca_error_fmt).
+-module(alpaca_error_format).
 
 -export([fmt/2]).
 
