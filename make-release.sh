0ac8bc9dd3bcbb6ef0138c7b3cc2a6f9fbb2bed4
Jeremy Pierre <j.14159@gmail.com>
2017-07-12 16:30:07 -0700

    Simple tgz release building and publishing

---
+++ make-release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# If travis-ci is building a tag then use that as the version, otherwise mark
+# this release with the current git hash:
+VERSION=${TRAVIS_TAG:-`git rev-parse HEAD`}
+
+# Where we're copying .beam files from:
+ALPACA_BEAMS=_build/test/lib/alpaca/ebin
+
+echo "Building release with version $VERSION"
+
+REL_BASE=alpaca-$VERSION
+BEAM_TARGET=$REL_BASE/beams
+SRC_TARGET=$REL_BASE/src
+
+mkdir -p $BEAM_TARGET
+mkdir -p $SRC_TARGET
+
+cp README.md $REL_BASE
+cp Tour.md $REL_BASE
+cp code_of_conduct.md $REL_BASE
+cp $ALPACA_BEAMS/*.beam $BEAM_TARGET
+cp src/* $SRC_TARGET
+
+tar cvzf alpaca.tgz $REL_BASE
+
